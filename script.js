b354c0192102bcb839891f84ac5b11404f06b7d4
rupalisarcar <rupalisarcar@gmail.com>
2025-11-06 09:06:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	console.log("test")
+	const table = document.getElementById("sampleTable");
+	
+	const tr = table.insertRow(0);//document.createElement("tr");
+	const firstCell = tr.insertCell(0)//document.createElement("td");
+	firstCell.textContent = 'New Cell1';
+	const secondCell = tr.insertCell(1)//document.createElement("td");
+	secondCell.textContent = 'New Cell2';
+	// tr.appendChild(firstCell);
+	// tr.appendChild(secondCell);
+	// table.appendChild(tr);  
 }
